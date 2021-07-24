e3823f2a6e293b46667bd0b997806029cfebdfa9
Luca <mastrantonio.luca.91@icloud.com>
2021-07-24 12:35:51 +0200

    Main - bottom structure

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -9,3 +9,15 @@
 // Categoria selezionata dall'utente
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
 
+// ACQUISIZIONE DATI
+var btnGenera = document.getElementById('genera');
+
+var km_price = 0.21;
+
+btnGenera.addEventListener('click',
+    function() {
+        var nome = document.getElementById('nome').value;
+        var km = document.getElementById('km').value;
+        var select_eta = document.getElementById('eta').value;
+    }
+)
